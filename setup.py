9f8305445c7890dc5d77c23c3ada0c3f5ed69085
Ryan Carey <rcarey@dataxu.com>
2016-02-29 13:10:37 -0500

    fix python 2.6 dependencies to accept newer versions

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ if sys.version_info[:2] == (2, 6):
     # For python2.6 we have a few other dependencies.
     # First we need an ordered dictionary so we use the
     # 2.6 backport.
-    requires.append('ordereddict==1.1')
+    requires.append('ordereddict>=1.1')
     # Then we need simplejson.  This is because we need
     # a json version that allows us to specify we want to
     # use an ordereddict instead of a normal dict for the
     # JSON objects.  The 2.7 json module has this.  For 2.6
     # we need simplejson.
-    requires.append('simplejson==3.3.0')
+    requires.append('simplejson>=3.3.0')
 
 
 setup(
@@ -38,8 +38,8 @@ setup(
     install_requires=requires,
     extras_require={
         ':python_version=="2.6"': [
-            'ordereddict==1.1',
-            'simplejson==3.3.0',
+            'ordereddict>=1.1',
+            'simplejson>=3.3.0',
         ]
     },
     license="Apache License 2.0",
